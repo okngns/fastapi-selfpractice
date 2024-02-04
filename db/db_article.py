ad06f945354356e3f0e65ecdb3df22bf7d06699d
Okan Güneş <okngns1@outlook.com>
2024-02-04 15:26:32 +0100

    new db

---
+++ db/db_article.py
@@ -0,0 +1,37 @@
+from exceptions import StoryException
+from sqlalchemy.orm.session import Session
+from db.models import DbArticle
+from schemas import ArticleBase
+from fastapi import HTTPException, status, Depends
+from schemas import ArticleDisplay
+from db.database import get_db
+
+def create_article(db: Session, request: ArticleBase):
+  if request.content.startswith('Once upon a time'):
+      raise StoryException('No stories please')
+  new_article = DbArticle(
+    title = request.title,
+    content = request.content,
+    published = request.published,
+    user_id = request.creator_id
+  )
+  db.add(new_article)
+  db.commit()
+  db.refresh(new_article)
+  return new_article
+
+def get_article(db: Session, id: int = None):
+    article = db.query(DbArticle).filter(DbArticle.id == id).first()
+    if not article:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Article with id {id} not found",
+        )
+    try:
+        return article
+    except Exception as e:
+        print(f"Error during Pydantic model conversion: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error"
+        )
