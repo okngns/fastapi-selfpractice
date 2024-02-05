5191fe4ebfe32648d69f7d2493c553cefce946b9
Okan Güneş <okngns1@outlook.com>
2024-02-05 09:53:07 +0100

    securing an endpoint

--- router/article.py
+++ router/article.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_article
+from auth.oauth2 import oauth2_schema
 
 router = APIRouter(
     prefix='/article',
@@ -17,7 +18,7 @@ def create_article(request: ArticleBase, db: Session= Depends(get_db)):
 
 #get specific article
 @router.get('/{id}') #, response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db)):
+def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return{
       'data' : db_article.get_article(db, id)
     }  
